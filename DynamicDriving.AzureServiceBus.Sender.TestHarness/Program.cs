c155fb3aa536052e7411d460bf192a862c2a3c9f
christian aranda <kristian.aranda@gmail.com>
2022-02-12 21:24:47 +0100

    add testharness

---
+++ DynamicDriving.AzureServiceBus.Sender.TestHarness/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DynamicDriving.AzureServiceBus;
+using DynamicDriving.Events;
+
+Console.WriteLine("Press a key to send a message through Azure Service Bus");
+Console.ReadKey();
+
+var serviceBus = new AzureServiceBusMessagePublisher(new AzureServiceBusOptions
+{
+    StorageConnectionString = "Endpoint=sb://dynamic-driving.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Nrf+Eohs7r03wkddo9LHQf+pfyunP+WvBVe0PaotqOw="
+});
+
+await serviceBus.PublishAsync(new TestEvent(Guid.NewGuid(), "test 01", "1"));
+
+public record TestEvent(Guid Id, string Name, string Value) : IIntegrationEvent;
