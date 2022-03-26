c04f3cdddfb192d4cebed6fffa62b0305cb47781
christian aranda <kristian.aranda@gmail.com>
2022-03-26 18:28:47 +0100

    add context

---
+++ Services/Trips/DynamicDriving.TripManagement.Infrastructure/TripManagementContext.cs
@@ -0,0 +1,25 @@
+﻿using DynamicDriving.SharedKernel.DomainDriven;
+using DynamicDriving.TripManagement.Domain.DriversAggregate;
+using DynamicDriving.TripManagement.Domain.LocationsAggregate;
+using DynamicDriving.TripManagement.Domain.TripsAggregate;
+using DynamicDriving.TripManagement.Domain.UsersAggregate;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DynamicDriving.TripManagement.Infrastructure;
+
+public sealed class TripManagementContext : TransactionContext
+{
+    public TripManagementContext(DbContextOptions options, IMediator mediator) 
+        : base(options, mediator)
+    {
+    }
+
+    public DbSet<Trip> Trips { get; set; } = default!;
+
+    public DbSet<Location> Locations { get; set; } = default!;
+
+    public DbSet<User> Users { get; set; } = default!;
+
+    public DbSet<Driver> Drivers { get; set; } = default!;
+}
