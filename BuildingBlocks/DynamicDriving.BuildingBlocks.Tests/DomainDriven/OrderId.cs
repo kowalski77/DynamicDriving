0494eedd43a8e96187bd066b869b169e05f71dcf
christian aranda <kristian.aranda@gmail.com>
2022-04-03 20:48:03 +0200

    refactor stronlytypeid concept

---
+++ BuildingBlocks/DynamicDriving.BuildingBlocks.Tests/DomainDriven/OrderId.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DynamicDriving.BuildingBlocks.Tests.DomainDriven;
+
+public readonly struct OrderId : IEquatable<OrderId>
+{
+    public OrderId()
+    {
+        this.Value = Guid.NewGuid();
+    }
+
+    public OrderId(Guid value)
+    {
+        this.Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public bool Equals(OrderId other)
+    {
+        return this.Value.Equals(other.Value);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        return obj is OrderId other && this.Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.Value.GetHashCode();
+    }
+
+    public static bool operator ==(OrderId left, OrderId right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(OrderId left, OrderId right)
+    {
+        return !Equals(left, right);
+    }
+}
---
+++ BuildingBlocks/DynamicDriving.BuildingBlocks.Tests/DomainDriven/StronglyTypeIdTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace DynamicDriving.BuildingBlocks.Tests.DomainDriven;
+
+public class StronglyTypeIdTests
+{
+    [Fact]
+    public void Same_values_are_equal()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var orderId1 = new OrderId(id);
+        var orderId2 = new OrderId(id);
+
+        // Act
+        var result = orderId1.Equals(orderId2);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Different_values_are_not_equal()
+    {
+        // Arrange
+        var orderId1 = new OrderId(Guid.NewGuid());
+        var orderId2 = new OrderId(Guid.NewGuid());
+
+        // Act
+        var result = orderId1.Equals(orderId2);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equal_operator_works_correctly()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var orderId1 = new OrderId(id);
+        var orderId2 = new OrderId(id);
+
+        // Act
+        var result = orderId1 == orderId2;
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void NotEqual_operator_works_correctly()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var orderId1 = new OrderId(id);
+        var orderId2 = new OrderId(id);
+
+        // Act
+        var result = orderId1 != orderId2;
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Parameterless_constructor_initializes_value_with_new_guid()
+    {
+        // Arrange
+        var orderId = new OrderId();
+
+        // Act
+        var result = orderId.Value;
+
+        // Assert
+        result.Should().NotBeEmpty();
+    }
+}
