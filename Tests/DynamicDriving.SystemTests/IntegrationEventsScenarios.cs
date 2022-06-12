66a1df15591f2b1f5dbbafd908109ad737ffde6c
christian aranda <kristian.aranda@gmail.com>
2022-06-12 19:38:55 +0200

    IClassFixture

--- Tests/DynamicDriving.SystemTests/IntegrationEventsScenarios.cs
+++ Tests/DynamicDriving.SystemTests/IntegrationEventsScenarios.cs
@@ -1,15 +1,23 @@
 ﻿using System.Net.Http.Json;
 using AutoFixture;
 using DynamicDriving.Models;
+using DynamicDriving.SystemTests.Services;
 using DynamicDriving.SystemTests.Support;
 using FluentAssertions;
 
-namespace DynamicDriving.SystemTests.Services;
+namespace DynamicDriving.SystemTests;
 
-public class IntegrationEventsScenarios
+public class IntegrationEventsScenarios : IClassFixture<WebApplicationFixture>
 {
     private const string DriversEndpoint = "/api/v1/Drivers";
     private readonly IFixture fixture = new Fixture();
+
+    private readonly WebApplicationFixture webApplicationFixture;
+
+    public IntegrationEventsScenarios(WebApplicationFixture webApplicationFixture)
+    {
+        this.webApplicationFixture = webApplicationFixture;
+    }
 
     [Fact]
     public async Task Driver_is_created_in_trip_management_when_is_registered()
@@ -18,11 +26,8 @@ public class IntegrationEventsScenarios
         var driverId = Guid.NewGuid();
         var request = this.fixture.Build<RegisterDriverRequest>().With(x => x.Id, driverId).Create();
 
-        await using var drivers = new DriversWebApplicationFactory();
-        await using var trips = new TripsWebApplicationFactory();
-
         // Act
-        var response = await drivers.HttpClient.PostAsJsonAsync(DriversEndpoint, request);
+        var response = await this.webApplicationFixture.Drivers.HttpClient.PostAsJsonAsync(DriversEndpoint, request);
 
         // Assert
         response.EnsureSuccessStatusCode();
@@ -30,7 +35,7 @@ public class IntegrationEventsScenarios
         await Retry.Handle<InvalidOperationException>(1000, 5)
             .ExecuteAsync(async () =>
             {
-                var driver = await trips.GetDriverByIdAsync(driverId);
+                var driver = await this.webApplicationFixture.Trips.GetDriverByIdAsync(driverId);
                 driver.Name.Should().Be(request.Name);
             });
     }
---
+++ Tests/DynamicDriving.SystemTests/Services/WebApplicationFixture.cs
@@ -0,0 +1,20 @@
+﻿namespace DynamicDriving.SystemTests.Services;
+
+public sealed class WebApplicationFixture : IDisposable
+{
+    public WebApplicationFixture()
+    {
+        this.Drivers = new DriversWebApplicationFactory();
+        this.Trips = new TripsWebApplicationFactory();
+    }
+
+    public DriversWebApplicationFactory Drivers { get; }
+
+    public TripsWebApplicationFactory Trips { get; }
+
+    public void Dispose()
+    {
+        this.Drivers.Dispose();
+        this.Trips.Dispose();
+    }
+}
