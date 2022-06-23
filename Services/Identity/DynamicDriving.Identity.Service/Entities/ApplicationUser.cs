7d481fedd2a74fa44abb0eef8f989dd966b2f9ac
christian aranda <kristian.aranda@gmail.com>
2022-06-23 19:19:33 +0200

    DynamicDriving.Identity.Service Part 1

---
+++ Services/Identity/DynamicDriving.Identity.Service/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace DynamicDriving.Identity.Service.Entities;
+
+[CollectionName("Users")]
+public class ApplicationUser : MongoIdentityUser<Guid>
+{
+}
