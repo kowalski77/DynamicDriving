4151109344cb6e1c5877638409bd221a1724fc2b
Christian Aranda <kristian.aranda@gmail.com>
2022-06-25 16:06:48 +0200

    Finish Identity Service

---
+++ Services/Identity/DynamicDriving.Identity.Service/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using DynamicDriving.Identity.Service.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicDriving.Identity.Service.Controllers;
+
+[ApiController]
+[Route("users")]
+public class UsersController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> userManager;
+
+    public UsersController(UserManager<ApplicationUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<UserDto>> Get()
+    {
+        var users = this.userManager.Users.Select(users => users.AsDto()).ToList();
+
+        return this.Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> Get(Guid id)
+    {
+        var user = await this.userManager.FindByIdAsync(id.ToString());
+
+        return user is null ?
+            this.NotFound() :
+            this.Ok(user.AsDto());
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(Guid id, UpdateUserDto user)
+    {
+        var userToUpdate = await this.userManager.FindByIdAsync(id.ToString());
+        if (userToUpdate is null)
+        {
+            return this.NotFound();
+        }
+
+        userToUpdate.Email = user.Email;
+        userToUpdate.UserName = user.Email;
+        var result = await this.userManager.UpdateAsync(userToUpdate);
+
+        return result.Succeeded ?
+            this.NoContent() :
+            this.BadRequest(result.Errors);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var userToDelete = await this.userManager.FindByIdAsync(id.ToString());
+        if (userToDelete is null)
+        {
+            return this.NotFound();
+        }
+
+        var result = await this.userManager.DeleteAsync(userToDelete);
+
+        return result.Succeeded ?
+            this.NoContent() :
+            this.BadRequest(result.Errors);
+    }
+}
---
+++ Services/Identity/DynamicDriving.Identity.Service/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using DynamicDriving.Identity.Service.Entities;
+
+namespace DynamicDriving.Identity.Service;
+
+public static class Extensions
+{
+    public static UserDto AsDto(this ApplicationUser user)
+    {
+        return new UserDto(
+            user.Id,
+             user.UserName,
+             user.Email,
+             user.CreatedOn);
+    }
+}
