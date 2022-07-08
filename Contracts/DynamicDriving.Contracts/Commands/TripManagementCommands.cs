b291486f3cf8fc0f167be8358a4b4ffb60230c30
Christian Aranda <kristian.aranda@gmail.com>
2022-07-08 20:02:01 +0200

    add invalidate consumer

--- Contracts/DynamicDriving.Contracts/Commands/TripManagementCommands.cs
+++ Contracts/DynamicDriving.Contracts/Commands/TripManagementCommands.cs
@@ -1,3 +1,5 @@
 ﻿namespace DynamicDriving.Contracts.Commands;
 
 public record ConfirmTrip(Guid TripId, Guid CorrelationId);
+
+public record InvalidateTrip(Guid TripId, Guid CorrelationId);
---
+++ Services/Trips/DynamicDriving.TripManagement.API/UseCases/Trips/Confirm/InvalidateTripConsumer.cs
@@ -0,0 +1,26 @@
+﻿using DynamicDriving.SharedKernel;
+using MassTransit;
+using MediatR;
+using InvalidateTrip = DynamicDriving.Contracts.Commands.InvalidateTrip;
+using InvalidateTripCommand = DynamicDriving.TripManagement.Application.Trips.Commands.InvalidateTrip;
+
+namespace DynamicDriving.TripManagement.API.UseCases.Trips.Confirm;
+
+public class InvalidateTripConsumer : IConsumer<InvalidateTrip>
+{
+    private readonly IMediator mediator;
+
+    public InvalidateTripConsumer(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    public async Task Consume(ConsumeContext<InvalidateTrip> context)
+    {
+        Guards.ThrowIfNull(context);
+
+        var command = new InvalidateTripCommand(context.Message.TripId, context.Message.CorrelationId);
+
+        await this.mediator.Send(command).ConfigureAwait(false);
+    }
+}
