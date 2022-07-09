ecf22802e5b79f3b3faad4ced576f3c2a8d20cf8
Christian Aranda <kristian.aranda@gmail.com>
2022-07-09 19:06:32 +0200

    TripInvalidatedConsumer

---
+++ Services/Drivers/DynamicDriving.DriverManagement.API/UseCases/Trips/Create/TripInvalidatedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using DynamicDriving.Contracts.Events;
+using DynamicDriving.DriverManagement.Core.Trips.Commands;
+using DynamicDriving.SharedKernel;
+using DynamicDriving.SharedKernel.Application;
+using DynamicDriving.SharedKernel.Results;
+using MassTransit;
+
+namespace DynamicDriving.DriverManagement.API.UseCases.Trips.Create;
+
+public class TripInvalidatedConsumer : IConsumer<TripInvalidated>
+{
+    private readonly IServiceCommand<InvalidateTrip, Result> serviceCommand;
+
+    public TripInvalidatedConsumer(IServiceCommand<InvalidateTrip, Result> serviceCommand)
+    {
+        this.serviceCommand = serviceCommand;
+    }
+
+    public async Task Consume(ConsumeContext<TripInvalidated> context)
+    {
+        Guards.ThrowIfNull(context);
+
+        var command = new InvalidateTrip(context.Message.TripId);
+
+        await this.serviceCommand.ExecuteAsync(command).ConfigureAwait(false);
+    }
+}
---
+++ Services/Drivers/DynamicDriving.DriverManagement.Core/Trips/Commands/InvalidateTrip.cs
@@ -0,0 +1,26 @@
+﻿using DynamicDriving.SharedKernel;
+using DynamicDriving.SharedKernel.Application;
+using DynamicDriving.SharedKernel.Results;
+
+namespace DynamicDriving.DriverManagement.Core.Trips.Commands;
+
+public record InvalidateTrip(Guid TripId) : ICommand<Result>;
+
+public sealed record InvalidateTripCommand : IServiceCommand<InvalidateTrip, Result>
+{
+    private readonly ITripRepository tripRepository;
+
+    public InvalidateTripCommand(ITripRepository tripRepository)
+    {
+        this.tripRepository = tripRepository;
+    }
+
+    public async Task<Result> ExecuteAsync(InvalidateTrip command, CancellationToken cancellationToken = default)
+    {
+        Guards.ThrowIfNull(command);
+
+        await this.tripRepository.RemoveAsync(command.TripId, cancellationToken).ConfigureAwait(false);
+
+        return Result.Ok();
+    }
+}
